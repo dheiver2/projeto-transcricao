1f9ec341d372382a8499e983d309550862030a13
Dheiver Francisco Santos <165256830+dheiver2@users.noreply.github.com>
2025-01-12 08:43:31 -0300

    Delete src/lib directory

--- src/lib/utils.js
+++
@@ -1,8 +0,0 @@
-// src/lib/utils.js
-// Utilitário para mesclar classes do Tailwind
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
